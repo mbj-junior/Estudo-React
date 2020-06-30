33e8f4a60671611812d0a8fe155cf76ff1076023
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-30 13:37:41 -0300

    Acrescentando os itens

---
+++ curso-react/src/Formulario.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+class Formulario extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.stateInicial = {
+            nome: '',
+            livro: '',
+            preco: '',
+        }
+
+        this.state = this.stateInicial;
+    }
+
+    escutandorDeInput = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    submitFormulario = () => {
+        this.props.escutadorDeSubmit(this.state);
+        this.setState(this.stateInicial);
+    }
+
+    render() {
+
+        const { nome, livro, preco } = this.state;
+
+        return (
+            <form>
+
+                <label htmlFor="nome">Nome</label>
+                <input
+                    id="nome"
+                    type="text"
+                    name="nome"
+                    value={nome}
+                    onChange={this.escutandorDeInput} />
+
+                <label htmlFor="livro">Livro</label>
+                <input
+                    id="livro"
+                    type="text"
+                    name="livro"
+                    value={livro}
+                    onChange={this.escutandorDeInput} />
+
+                <label htmlFor="preco">Preço</label>
+                <input
+                    id="preco"
+                    type="text"
+                    name="preco"
+                    value={preco}
+                    onChange={this.escutandorDeInput} />
+
+                <button onClick={this.submitFormulario} type="button">Salvar
+                </button>
+            </form>
+        )
+    }
+}
+
+export default Formulario;
+

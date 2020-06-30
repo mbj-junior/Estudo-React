6c92f21b812edbec20af53b398d48c49a125b2ab
Marcio Botelho Jr <mbj_junior@hotmail.com>
2020-06-30 09:41:53 -0300

    configurando o state

--- curso-react/src/App.js
+++ curso-react/src/App.js
@@ -1,37 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Tabela from './Tabela';
 
-function App() {
+class App extends Component {
 
-  const autores = [
-    {
-      nome: 'Paulo',
-      livro: 'React',
-      preco: '1000'
-    },
-    {
-      nome: 'Daniel',
-      livro: 'Java',
-      preco: '99'
-    },
-    {
-      nome: 'Marcos',
-      livro: 'Design',
-      preco: '150'
-    },
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '100'
-    }
-  ];
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      },
+      {
+        nome: 'Nico',
+        livro: 'Java',
+        preco: '89'
+      }
+    ],
+  };
 
-  return (
-    <div className="App">
-      <Tabela autores={autores}/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Tabela autores={this.state.autores} />
+      </div>
+    );
+  }
 }
 
 export default App;

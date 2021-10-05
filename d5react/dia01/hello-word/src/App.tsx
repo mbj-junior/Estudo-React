6ceb4418237249ce1b1c8be86bf1257e5c912b9d
Márcio Botelho Junior <marcio.botelhojunior@gmail.com>
2021-10-05 01:04:44 -0300

    useState

--- d5react/dia01/hello-word/src/App.tsx
+++ d5react/dia01/hello-word/src/App.tsx
@@ -3,12 +3,21 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
 const App = () => {
-  let name = "Marcinho";
-  let age = 99;
-
+  const [name, steName] = useState("Marcinho");
   const [bg, setBg] = useState("#FF0000");
+
+  const [age, setAge] = useState(90);
   const handleClick = () => {
-    alert("Clicou na função");
+    setBg("#0000FF");
+  };
+
+  const handle20 = () => {
+    setAge(20);
+    setBg("#00FF00")
+  };
+
+  const handle90 = () => {
+    setAge(90);
     setBg("#0000FF");
   };
 
@@ -17,6 +26,8 @@ const App = () => {
       <Header name={name} age={age} />
       Olá {name}, tudo bem? sua idade {age}
       <button onClick={handleClick}>Clique aqui</button>
+      {age === 90 && <button onClick={handle20}>20</button>}
+      {age === 20 && <button onClick={handle90}>90</button>}
       <Footer />
     </div>
   );
